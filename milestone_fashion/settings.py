16d588b0397ac476999c4f856d42fe2a7e78b176
NunoCos <nuno18_9@hotmail.com>
2021-08-28 18:06:34 +0000

    add add to bag functionality

--- milestone_fashion/settings.py
+++ milestone_fashion/settings.py
@@ -73,7 +73,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.context.bag_context',
             ],
         },
     },
@@ -160,7 +159,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-FREE_DELIVERY_TRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
