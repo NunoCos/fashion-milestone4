9cd2554d2a9d789292782b707cf2dadcf332ffee
NunoCos <nuno18_9@hotmail.com>
2021-09-01 23:16:44 +0000

    set allowed host and deploy to heroku

--- milestone_fashion/settings.py
+++ milestone_fashion/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(o*$%rw33ebb7a_q)=c)d&yc9i!%xg8dwz)f492#g9n&zd=9l=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-fashion.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -110,12 +110,17 @@ WSGI_APPLICATION = 'milestone_fashion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
