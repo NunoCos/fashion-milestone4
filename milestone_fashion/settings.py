0195cdf9f1101c3175776c820aad15c4a229efc3
NunoCos <nuno18_9@hotmail.com>
2021-09-02 09:06:56 +0000

    add checkout app models

--- milestone_fashion/settings.py
+++ milestone_fashion/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'fdu2fpvycovk=0)=(=zn0n&3-wu-(#yo6*(xvk3tm_oq3vwlkf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'shoppingbag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
